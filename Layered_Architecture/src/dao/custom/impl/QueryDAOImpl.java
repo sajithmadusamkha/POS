ee65ec6a58ffa135eb2e3192fa7f131555de41b5
sajithmadusamkha <sajithmadusankha02@gmail.com>
2022-05-14 20:26:23 +0530

    Introduce a place for adding join query

--- Layered_Architecture/src/dao/custom/impl/QueryDAOImpl.java
+++ Layered_Architecture/src/dao/custom/impl/QueryDAOImpl.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class QueryDAOImpl implements QueryDAO {
 
     @Override
-    public void searchByOrderId(String id) throws ClassNotFoundException, SQLException {
+    public void searchByOrderId(String id) throws ClassNotFoundException, SQLException{
         String sql = "select Orders.oid,Orders.date,Orders.customerID,OrderDetails.itemCode,OrderDetails.qty,OrderDetails.unitPrice from Orders inner join OrderDetails on Orders.oid=OrderDetails.oid where Orders.oid=?;";
     }
 }
